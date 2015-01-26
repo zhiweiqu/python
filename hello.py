fa9a94cadd1d3bbfac6345cd92d6991ce65a4764
kevin <kevin.qu326@gmail.com>
2015-01-26 16:26:21 +0800

    add hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#import modules used here
+import sys
+
+#sys.argv[0] is the script name itself and can be ignored
+#sys.argv[1] is the first argument
+def main():
+    print 'Hello', sys.argv[1], sys.argv[2]
+
+if __name__ == '__main__':
+    main()
